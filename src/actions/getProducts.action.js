c7db7a8260d09454c8a0fb9d48e644e716633d0f
Marco <mgenova03@gmail.com>
2019-04-28 12:55:27 -0400

    end of json input

--- src/actions/getProducts.action.js
+++ src/actions/getProducts.action.js
@@ -3,24 +3,23 @@ import getProductsQuery from '../queries/products/getProducts';
 
 import ApolloClient from 'apollo-boost';
 
-
-
 export function getProducts() {
   return async (dispatch, getState) => {
   	
     const client = new ApolloClient({
   		uri: `${process.env.REACT_APP_API_URL}graphql`
 	  });
-
-    // client.listen({ port: process.env.REACT_APP_API_URL || 5000 }).then(({ url }) => {
-    //   console.log(`🚀 Server ready at ${url}`);
-    // });
+    console.log(client)
 
     const request = await client.query({
       query: getProductsQuery,
     });
+    console.log(request)
 
     const result = await request.data;
+
+    console.log(result)
+
     dispatch({
       type: actionTypes.GET_PRODUCTS,
       payload: result
--- src/components/Products/ProductTable.js
+++ src/components/Products/ProductTable.js
@@ -9,19 +9,12 @@ export class ProductTable extends Component {
   render() {
 	const columns = ["ID", "Name", "City", "State"];
     
-    console.log(this.props.products)
 
 	const data = this.props.products.map(product => {
 	  return Object.values(product).slice(0,-1)
 	})
 	console.log(data)
 
-	// const data = [
-	//  ["Joe James", "Test Corp", "Yonkers", "NY"],
-	//  ["John Walsh", "Test Corp", "Hartford", "CT"],
-	//  ["Bob Herm", "Test Corp", "Tampa", "FL"],
-	//  ["James Houston", "Test Corp", "Dallas", "TX"],
-	// ];
 
 	// const { products } = this.props;
     return (
