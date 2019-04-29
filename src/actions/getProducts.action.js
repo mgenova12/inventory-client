e67636d8a261790e90be571fd9f19b42d629e9c7
Marco <mgenova03@gmail.com>
2019-04-29 14:31:14 -0400

    log graphql errors

--- src/actions/getProducts.action.js
+++ src/actions/getProducts.action.js
@@ -2,33 +2,41 @@ import actionTypes from "../actionTypes";
 import getProductsQuery from '../queries/products/getProducts';
 
 import { ApolloClient } from 'apollo-boost';
-import { HttpLink } from 'apollo-link-http'
+import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from "apollo-link-error";
+// `${process.env.REACT_APP_API_URL}graphql`
 
 export function getProducts() {
-    const cache = new InMemoryCache();
-    const client = new ApolloClient({
-      cache,
-      link: new HttpLink({
-        uri: `${process.env.REACT_APP_API_URL}graphql`,
-        opts: {
-          mode: 'no-cors'
-        }
-      }),
-    });
-    console.log('client')
-    console.log(client)
+  
+const link_errors = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
 
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+  const client = new ApolloClient({
+    link: createHttpLink({ uri: `${process.env.REACT_APP_API_URL}graphql` }),
+    cache: new InMemoryCache(),
+    link_errors
+  });
+
+  console.log('client')
+  console.log(client)
   return async (dispatch, getState) => {
     const request = await client.query({
       query: getProductsQuery,
     });
-    
+
     console.log('request')
     console.log(request)
 
     const result = await request.data;
-
     dispatch({
       type: actionTypes.GET_PRODUCTS,
       payload: result
