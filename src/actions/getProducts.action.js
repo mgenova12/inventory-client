07600c776f0cda77a5c5399c3bcc45dee1f1c85d
Marco <mgenova03@gmail.com>
2019-04-29 15:11:48 -0400

    add content type

--- src/actions/getProducts.action.js
+++ src/actions/getProducts.action.js
@@ -4,27 +4,32 @@ import getProductsQuery from '../queries/products/getProducts';
 import { ApolloClient } from 'apollo-boost';
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { onError } from "apollo-link-error";
-import { ApolloLink } from 'apollo-link'
+// import { onError } from "apollo-link-error";
+// import { ApolloLink } from 'apollo-link'
 // `${process.env.REACT_APP_API_URL}graphql`
 
 export function getProducts() {
   
-const link_errors = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.map(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-      ),
-    );
+// const link_errors = onError(({ graphQLErrors, networkError }) => {
+//   if (graphQLErrors)
+//     graphQLErrors.map(({ message, locations, path }) =>
+//       console.log(
+//         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+//       ),
+//     );
 
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
+//   if (networkError) console.log(`[Network error]: ${networkError}`);
+// });
 
-  const client = new ApolloClient({
-    link: ApolloLink.from([link_errors,createHttpLink({ uri: `${process.env.REACT_APP_API_URL}graphql.json` }), ]),
-    cache: new InMemoryCache(),
-  });
+const client = new ApolloClient({
+  link: new createHttpLink({
+    uri: `${process.env.REACT_APP_API_URL}graphql`,
+    headers: {
+      'Content-Type': 'application/graphql',
+    }
+  }),
+  cache: new InMemoryCache(),
+})
 
   console.log('client')
   console.log(client)
