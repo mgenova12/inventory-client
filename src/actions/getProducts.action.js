67a555ac0257c64c77acbf956f4985c2f1e191d8
Marco <mgenova03@gmail.com>
2019-04-29 12:59:34 -0400

    log query request

--- src/actions/getProducts.action.js
+++ src/actions/getProducts.action.js
@@ -2,44 +2,27 @@ import actionTypes from "../actionTypes";
 import getProductsQuery from '../queries/products/getProducts';
 
 import { ApolloClient } from 'apollo-boost';
-import { createHttpLink } from "apollo-link-http"
+import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export function getProducts() {
-  return async (dispatch, getState) => {
-
-  const customFetch = (uri, options) => {
-    return fetch(uri, options)
-    .then(response => {
-      if (response.status >= 500) {  // or handle 400 errors
-        return Promise.reject(response.status);
-      }
-      return response;
+    const cache = new InMemoryCache();
+    const client = new ApolloClient({
+      cache,
+      link: new HttpLink({
+        uri: `${process.env.REACT_APP_API_URL}graphql`,
+      }),
     });
-  };
-
-  const client = new ApolloClient({
-    link: createHttpLink({
-      uri: `${process.env.REACT_APP_API_URL}graphql`,
-      fetch: customFetch,
-    }),
-    cache: new InMemoryCache()
-  });
-
-
-
-    // const cache = new InMemoryCache();
-    // const client = new ApolloClient({
-    //   cache,
-    //   link: new HttpLink({
-    //     uri: `${process.env.REACT_APP_API_URL}graphql`,
-    //   }),
-    // });
-
-
+    console.log('client')
+    console.log(client)
+    
+  return async (dispatch, getState) => {
     const request = await client.query({
       query: getProductsQuery,
     });
+    
+    console.log('request')
+    console.log(request)
 
     const result = await request.data;
 
