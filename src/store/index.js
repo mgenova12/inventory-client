26751a44f483738c181183c74b2f10a47665eb5e
Marco <mgenova03@gmail.com>
2019-05-01 13:04:12 -0400

    add dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,18 @@ import thunk from "redux-thunk";
 
 import rootReducer from "../reducers";
 
-const allStoreEnhancers = compose( 
-	applyMiddleware(thunk)
-	// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const allStoreEnhancers = compose( 
+// 	applyMiddleware(thunk)
+// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
-//check if production?
+const middleware = applyMiddleware(thunk);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
 	rootReducer,
-	allStoreEnhancers
+	composeEnhancers(middleware)
 )
 
 export default store;
